2d03e69375955795d30cc0117dbcdc351f74b0d9
xianning liu <xianlinbox@gmail.com>
2023-03-30 16:59:26 -0500

    xianniing: adjust code to team three

--- environments/nonprod-us-east-2.auto.tfvars.json.tpl
+++ environments/nonprod-us-east-2.auto.tfvars.json.tpl
@@ -2,7 +2,7 @@
   "cluster_name": "sandbox-us-east-2",
   "aws_region": "us-east-2",
   "assume_role": "DPSPlatformVPCRole",
-  "account_id": "{{ op://empc-lab/aws-dps-2/aws-account-id }}",
+  "account_id": "{{ op://cohorts/three-aws/aws-account-id }}",
   "vpc_cidr": "10.50.0.0/16",
   "vpc_azs": ["us-east-2a","us-east-2b","us-east-2c"],
   "vpc_private_subnets": ["10.50.0.0/19", "10.50.32.0/19", "10.50.64.0/19"],
--- versions.tf
+++ versions.tf
@@ -1,32 +1,32 @@
 terraform {
-  required_version = "~> 1.1"
+  required_version = "~> 1.3"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "twdps"
+    organization = "twks-cohort"
     workspaces {
-      prefix = "lab-platform-vpc-"
+      prefix = "three-lab-platform-vpc-"
     }
   }
 }
 
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
   assume_role {
     role_arn     = "arn:aws:iam::${var.account_id}:role/${var.assume_role}"
-    session_name = "lab-platform-vpc-${var.cluster_name}"
+    session_name = "three-lab-platform-vpc-${var.cluster_name}"
   }
 
   default_tags {
     tags = {
-      env      = var.cluster_name
-      cluster  = var.cluster_name
-      pipeline = "lab-platform-vpc"
+      env                                         = var.cluster_name
+      cluster                                     = var.cluster_name
+      pipeline                                    = "three-lab-platform-vpc"
       "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     }
   }
